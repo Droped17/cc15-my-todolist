864b2276bbeefe00a3246923ba0f8f7eb4e6d336
Droped17 <droped17@gmail.com>
2023-08-29 15:21:02 +0700

    finish step4: app-layout

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
+import './app.scss';
+
 function App() {
   return (
-    <div className="App">
-      React-App
+    <div className="todo">
+      <div className="todo__header">Header</div>
+      <div className="todo__sidebar">SideBar</div>
+      <div className="todo__content">TodoContent</div>
     </div>
   );
 }
